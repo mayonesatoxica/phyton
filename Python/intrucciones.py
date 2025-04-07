5e117c5e712dc9659c35cb2bc8c98b87169a59f7
miguel <164025473+mayonesatoxica@users.noreply.github.com>
2025-04-07 10:46:45 -0400

    Add files via upload

---
+++ Python/intrucciones.py
@@ -0,0 +1,23 @@
+#miguel cabezas
+#07-04-2025
+
+#instrucciones else
+
+contador = 0
+
+while contador < 10:
+    print('contador')
+    contador +=1
+    
+else:
+    print('dejo de contar')
+    
+contador = 0
+while contador < 5:
+    contador +=1
+        
+    if (contador == 4):
+        print('se rompio el bucle')
+        break
+    
+    print('contador')
