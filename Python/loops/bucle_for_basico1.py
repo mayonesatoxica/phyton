b51de2dc1852cf01541334759242510ad2c0f694
miguel <164025473+mayonesatoxica@users.noreply.github.com>
2025-04-08 09:17:13 -0400

    Add files via upload

---
+++ Python/loops/bucle_for_basico1.py
@@ -0,0 +1,35 @@
+#miguel cabezas
+#08-04-2025
+#tengo hambre
+
+print('basico')
+for i in range(101):
+    print(1)
+
+print('contando Vanilla Ice')
+for i in range(1, 101):
+    if i % 10 == 0:
+        print('baby')
+    elif i % 5 == 0:
+        print('ice ice')
+    else:
+        print(1)
+        
+        print('\nWow. numero gigante a la vista')
+        suma =(i for i in range(0, 500001, 2))
+        print(suma)
+        
+        
+        print('\Regresame al 3')
+        for i in range(2024, 0, -3):
+            print(i)
+        
+        numinicial = 3
+        numfinal = 100
+        multiplo = 2
+        
+        print('\ncontador dinamico')
+        for i in range(numinicial, numfinal + 1):
+            if 1 % multiplo ==0:
+                print(1)
+        
