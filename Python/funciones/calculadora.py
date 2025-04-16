aeab4c803f5d26ff352aae023b40fc67747e71d2
miguel <164025473+mayonesatoxica@users.noreply.github.com>
2025-04-16 11:15:48 -0400

    Add files via upload

---
+++ Python/funciones/calculadora.py
@@ -0,0 +1,20 @@
+#miguel cabezas
+#16-04-2025
+
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: División por cero no permitida"
+
+def potencia(a, b):
+    return a ** b
