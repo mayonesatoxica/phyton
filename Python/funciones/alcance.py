e6b91b76ba0c4dc18e3477a1dbe00b5ada084b5d
miguel <164025473+mayonesatoxica@users.noreply.github.com>
2025-04-16 12:15:51 -0400

    Add files via upload

---
+++ Python/funciones/alcance.py
@@ -0,0 +1,19 @@
+#miguel cabezas
+#16-04-2025
+
+def sumar(x,y):
+    respuesta = x+y
+    return respuesta
+
+print(sumar(4,2))
+
+#global
+
+respuesta = 0
+
+def sumar (x,y):
+    respuesta = x + y
+    return respuesta
+
+print(sumar(5, 10))
+
